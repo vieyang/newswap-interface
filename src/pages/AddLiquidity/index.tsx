b21c42733635b01b7ccc621f239f61c7b5f318df
Meng Guang <mengguang@newtonproject.org>
2020-09-25 12:48:14 +0800

    add more translation for confirm swap modal

--- src/pages/AddLiquidity/index.tsx
+++ src/pages/AddLiquidity/index.tsx
@@ -246,7 +246,11 @@ export default function AddLiquidity({
           </Text>
         </Row>
         <TYPE.italic fontSize={12} textAlign="left" padding={'8px 0 0 0 '}>
-          { t('Output is estimated. If the price changes by more than') + ' ' + (allowedSlippage / 100) + '% ' + t('your transaction will revert.')}
+          {t('Output is estimated. If the price changes by more than') +
+            ' ' +
+            allowedSlippage / 100 +
+            '% ' +
+            t('your transaction will revert.')}
         </TYPE.italic>
       </AutoColumn>
     )
