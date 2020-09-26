bddfd04e5bbf2a94a6beee0b4d5b0b86284a08e1
Meng Guang <mengguang@newtonproject.org>
2020-09-26 19:42:00 +0800

    comment out useless code.

--- src/components/swap/confirmPriceImpactWithoutFee.ts
+++ src/components/swap/confirmPriceImpactWithoutFee.ts
@@ -1,5 +1,5 @@
 import { Percent } from '@uniswap/sdk'
-import { ALLOWED_PRICE_IMPACT_HIGH, PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN } from '../../constants'
+//import { ALLOWED_PRICE_IMPACT_HIGH, PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN } from '../../constants'
 
 /**
  * Given the price impact, get user confirmation.
@@ -8,20 +8,20 @@ import { ALLOWED_PRICE_IMPACT_HIGH, PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN } from 
  */
 export default function confirmPriceImpactWithoutFee(priceImpactWithoutFee: Percent): boolean {
   return true
-  if (!priceImpactWithoutFee.lessThan(PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN)) {
-    return (
-      window.prompt(
-        `This swap has a price impact of at least ${PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN.toFixed(
-          0
-        )}%. Please type the word "confirm" to continue with this swap.`
-      ) === 'confirm'
-    )
-  } else if (!priceImpactWithoutFee.lessThan(ALLOWED_PRICE_IMPACT_HIGH)) {
-    return window.confirm(
-      `This swap has a price impact of at least ${ALLOWED_PRICE_IMPACT_HIGH.toFixed(
-        0
-      )}%. Please confirm that you would like to continue with this swap.`
-    )
-  }
-  return true
+  // if (!priceImpactWithoutFee.lessThan(PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN)) {
+  //   return (
+  //     window.prompt(
+  //       `This swap has a price impact of at least ${PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN.toFixed(
+  //         0
+  //       )}%. Please type the word "confirm" to continue with this swap.`
+  //     ) === 'confirm'
+  //   )
+  // } else if (!priceImpactWithoutFee.lessThan(ALLOWED_PRICE_IMPACT_HIGH)) {
+  //   return window.confirm(
+  //     `This swap has a price impact of at least ${ALLOWED_PRICE_IMPACT_HIGH.toFixed(
+  //       0
+  //     )}%. Please confirm that you would like to continue with this swap.`
+  //   )
+  // }
+  // return true
 }
